c63631b645e1a1b2b912882543a41c1563e472cb
keshetjackson <keshetjackson@gmail.com>
2024-11-10 21:37:23 +0200

    fix: moved pages { reports, dashboard ,users, coupons  } to admin drc

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -2,10 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LoginPage } from "./pages/LoginPage";
 import { MainLayout } from "./components/layouts/mainLayout";
 import { CouponEntryPage } from "./pages/CouponEntryPage";
-import { DashboardPage } from "./pages/DashboardPage";
-import { ReportsPage } from "./pages/ReportsPage";
-import { UsersPage } from "./pages/UsersPage";
-import { CouponsPage } from "./pages/CouponsPage";
+import { DashboardPage } from "./pages/admin/DashboardPage";
+import { ReportsPage } from "./pages/admin/ReportsPage";
+import { UsersPage } from "./pages/admin/UsersPage";
+import { CouponsPage } from "./pages/admin/CouponsPage";
 
 
 export function AppRouter() {
