c9ffccf52739b3c438ecf535d6141d0d3e2b982f
keshetjackson <keshetjackson@gmail.com>
2024-11-10 22:00:03 +0200

    faet: set up react query util to simulate real life awaiting time , currently 0.5 sec

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
+import { Suspense, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './global.css'
 import { AppRouter } from './AppRouter'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/reactQuery'
 
 function App() {
 
   return (
-    <AppRouter/>
+    <QueryClientProvider client={queryClient}>
+    <Suspense fallback={<div>Loading...</div>}>
+      <AppRouter />
+    </Suspense>
+  </QueryClientProvider>
   )
 }
 
